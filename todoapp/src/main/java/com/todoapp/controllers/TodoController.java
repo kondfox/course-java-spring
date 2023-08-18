02036dfbebb1ae05632cfc18207bcf85fdbfea7d
Peter Tamas <peter.tamas@greenfoxacademy.com>
2023-08-18 19:31:59 +0200

    GET /todo

--- todoapp/src/main/java/com/todoapp/controllers/TodoController.java
+++ todoapp/src/main/java/com/todoapp/controllers/TodoController.java
@@ -1,14 +1,18 @@
 package com.todoapp.controllers;
 
+import com.todoapp.models.Todo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoController {
 
-  @GetMapping("/hello")
-  public String hello() {
-    return "hello";
+  @GetMapping("/todo")
+  public ResponseEntity<Todo> getTodo() {
+    Todo todo = new Todo("kutyat setaltatni", false);
+    return ResponseEntity.status(HttpStatus.OK).body(todo);
   }
 
 }
