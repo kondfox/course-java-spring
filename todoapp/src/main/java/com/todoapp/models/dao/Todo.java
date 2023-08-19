54025ba11a3454dff3c327706ba61c8656e9b3b9
Peter Tamas <peter.tamas@greenfoxacademy.com>
2023-08-19 17:34:32 +0200

    connect with H2 database

---
+++ todoapp/src/main/java/com/todoapp/models/dao/Todo.java
@@ -0,0 +1,53 @@
+package com.todoapp.models.dao;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  @Column(length = 100)
+  private String title;
+  private boolean isDone;
+
+  public Todo() {
+  }
+
+  public Todo(String title, boolean isDone) {
+    this.title = title;
+    this.isDone = isDone;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public boolean isDone() {
+    return isDone;
+  }
+
+  public void setDone(boolean done) {
+    isDone = done;
+  }
+
+}
